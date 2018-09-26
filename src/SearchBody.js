56000e3ed5550ff27cf944cd6d87e06569ce63a3
renan.santos <renan.santos@dcc.ufmg.br>
2018-09-26 17:06:05 -0300

    Lecture 31

--- src/SearchBody.js
+++ src/SearchBody.js
@@ -1,12 +1,12 @@
 import React from "react";
-import {View, ScrollView, Text} from "react-native";
+import {View, ScrollView, Text, Image} from "react-native";
+import {ListItem, List} from "native-base";
 
 class SearchBody extends React.Component {
 	render() {
 		const pokemon = this.props.data;
 
 		if(!pokemon){
-			console.log(`retornou`);
 			return <View/>
 		}
 
@@ -15,6 +15,23 @@ class SearchBody extends React.Component {
 				<Text style={styles.header}>
 					#{pokemon.id} - {pokemon.name.toUpperCase()}
 				</Text>
+				<View style={styles.viewStyle}>
+					<Image
+						source={{uri: pokemon.sprites.front_default}}
+						style={styles.img}
+					/>
+				</View>
+				<View style={styles.info}>
+					<ListItem itemDivider>
+						<Text style={{fontWeight: "bold"}}>Size</Text>
+					</ListItem>
+					<ListItem itemDivider>
+						<Text>Weight - {pokemon.weight}</Text>
+					</ListItem>
+					<ListItem itemDivider>
+						<Text>Height - {pokemon.height}</Text>
+					</ListItem>
+				</View>
 			</ScrollView>
 		);
 	}
@@ -25,6 +42,22 @@ const styles = {
 		fontSize: 30,
 		color: "red",
 		textAlign: "center",
+	},
+	viewStyle: {
+		justifyContent: "flex-start",
+		alignItems: "center",
+		flex: 1
+	},
+	img: {
+		height: 200,
+		width: 200,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	info: {
+		flex: 1,
+		backgroundColor: "white",
+		opacity: 0.8
 	}
 };
 
