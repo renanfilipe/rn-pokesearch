5d5758ac0ba84dad98647187ff557d4f7be605a7
renan.santos <renan.santos@dcc.ufmg.br>
2018-09-26 12:58:32 -0300

    Lecture 20

--- App.js
+++ App.js
@@ -1,21 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Expo from 'expo';
+import { View, Platform } from 'react-native';
+import { Button, Text } from 'native-base';
 
 export default class App extends React.Component {
+	state = {
+		isReady: false
+  };
+
+  async componentWillMount() {
+		await Expo.Font.loadAsync({
+			Roboto: require("native-base/Fonts/Roboto.ttf"),
+			Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+			Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf")
+		});
+		this.setState({ isReady: true });
+	}
+
   render() {
+	  if (!this.state.isReady) {
+		  return <Expo.AppLoading />;
+	  }
+
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
+        <Button>
+          <Text>Hello World</Text>
+        </Button>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
+const styles = {
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: Platform.OS === 'android' ? 24 : 0,
   },
-});
+};
