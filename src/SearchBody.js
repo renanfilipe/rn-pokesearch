ae2a81902023dbb3cf0780065b164c1dcd6142b9
renan.santos <renan.santos@dcc.ufmg.br>
2018-09-26 17:29:15 -0300

    Lecture 32

--- src/SearchBody.js
+++ src/SearchBody.js
@@ -1,6 +1,9 @@
 import React from "react";
-import {View, ScrollView, Text, Image} from "react-native";
+import {View, ScrollView, Text, Image, Dimensions, ImageBackground} from "react-native";
 import {ListItem, List} from "native-base";
+
+const height = Dimensions.get("window").height;
+const width = Dimensions.get("window").width;
 
 class SearchBody extends React.Component {
 	render() {
@@ -11,33 +14,55 @@ class SearchBody extends React.Component {
 		}
 
 		return (
-			<ScrollView contentContainerStyle={{flex: 1}}>
-				<Text style={styles.header}>
-					#{pokemon.id} - {pokemon.name.toUpperCase()}
-				</Text>
-				<View style={styles.viewStyle}>
-					<Image
-						source={{uri: pokemon.sprites.front_default}}
-						style={styles.img}
-					/>
-				</View>
-				<View style={styles.info}>
-					<ListItem itemDivider>
-						<Text style={{fontWeight: "bold"}}>Size</Text>
-					</ListItem>
-					<ListItem itemDivider>
-						<Text>Weight - {pokemon.weight}</Text>
-					</ListItem>
-					<ListItem itemDivider>
-						<Text>Height - {pokemon.height}</Text>
-					</ListItem>
-				</View>
-			</ScrollView>
+			<ImageBackground
+				style={styles.backgroundImage}
+				source={{uri: "http://pokemongolive.com/img/posts/raids_loading.png"}}
+				resizeMode="cover"
+			>
+				<ScrollView style={{flex: 1}}>
+					<Text style={styles.header}>
+						#{pokemon.id} - {pokemon.name.toUpperCase()}
+					</Text>
+					<View style={styles.viewStyle}>
+						<Image
+							source={{uri: pokemon.sprites.front_default}}
+							style={styles.img}
+						/>
+					</View>
+					<View style={styles.info}>
+						<ListItem itemDivider>
+							<Text style={{fontWeight: "bold"}}>Size</Text>
+						</ListItem>
+						<ListItem>
+							<Text>Weight - {pokemon.weight}kg</Text>
+						</ListItem>
+						<ListItem>
+							<Text>Height - {pokemon.height/10}m</Text>
+						</ListItem>
+						<ListItem itemDivider>
+							<Text style={{fontWeight: "bold"}}>Abilities</Text>
+						</ListItem>
+						<List
+							dataArray={pokemon.abilities}
+							renderRow={(item) =>
+								<ListItem>
+									<Text>{item.ability.name}</Text>
+								</ListItem>
+							}
+						/>
+					</View>
+				</ScrollView>
+			</ImageBackground>
 		);
 	}
 }
 
 const styles = {
+	backgroundImage: {
+		flex: 1,
+		height: height,
+		width: width,
+	},
 	header: {
 		fontSize: 30,
 		color: "red",
